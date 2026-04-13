8610845259626370452e15ae9b8d1e79a0884d95
r3p-dev <r3p.dev@gmail.com>
2026-04-13 07:25:08 +0000

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import adonisjs from '@adonisjs/vite/client'
+import inertia from '@adonisjs/inertia/vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    inertia({ ssr: { enabled: false, entrypoint: 'inertia/ssr.tsx' } }),
+    adonisjs({ entrypoints: ['inertia/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+  ],
+
+  /**
+   * Define aliases for importing modules from
+   * your frontend code
+   */
+  resolve: {
+    alias: {
+      '@/': `${import.meta.dirname}/inertia/`,
+      '@generated': `${import.meta.dirname}/.adonisjs/client/`,
+    },
+  },
+
+  server: {
+    watch: {
+      ignored: ['**/storage/**', '**/tmp/**'],
+    },
+  },
+})
